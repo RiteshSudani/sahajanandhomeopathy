ae19ff11889e13e250e2a3400073f047e403496a
RiteshSudani <riteshsudani55@gmail.com>
2024-10-16 11:13:10 +0530

    text color change

---
+++ prectice.js
@@ -0,0 +1,35 @@
+
+
+
+// const demo = () => {
+
+//     let a =5;
+//     let b= 4;
+    
+
+//     const c= a+b;
+
+//     console.log("c=>",c)
+//     console.log("")
+
+// };
+
+// demo();
+
+// const num = prompt("enter your num")
+
+
+// const even = ()=>{
+
+
+
+//     if(num%2===0){
+//         console.log("this is even")
+//     }
+//     else{
+       
+//         console.log("this is odd")
+//     }
+// }    
+
+// even();
